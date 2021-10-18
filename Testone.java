bbafe66ac865fc5a40898f2b1692618e857bc3d0
priya-rani26 <priyarani2682002@gmail.com>
2021-10-18 09:14:22 +0530

    Adding two number

---
+++ Testone.java
@@ -0,0 +1,8 @@
+public class Testone {
+    public static void main(String[] args){
+        int a=2;
+        int b=5;
+        int c=a+b;
+        System.out.println("The sum of a and b is : "+ c);
+}
+}
