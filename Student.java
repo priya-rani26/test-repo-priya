ea638734f5fe3feed1eec80e6bea4b32c3e15765
priya-rani26 <priyarani2682002@gmail.com>
2021-10-20 15:19:17 +0530

    student file

---
+++ Student.java
@@ -0,0 +1,8 @@
+class Student{
+      Pubic  String displayName(){
+            String name="Priya Rani";
+            String name = Student.displayName();
+            System.out.println("my name is : ");
+            return nmae;
+      }
+}
