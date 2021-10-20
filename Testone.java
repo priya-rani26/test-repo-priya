5847e5152aacad23f00358c6e021e0e1b6092d8f
priya-rani26 <92663484+priya-rani26@users.noreply.github.com>
2021-10-20 15:37:56 +0530

    increment code

--- Testone.java
+++ Testone.java
@@ -1,8 +1,7 @@
 public class Testone {
     public static void main(String[] args){
         int a=2;
-        int b=5;
-        int c=a+b;
-        System.out.println("The sum of a and b is : "+ c);
+        System.out.println(a++);//2
+        System.out.println(++a);//4
 }
 }
