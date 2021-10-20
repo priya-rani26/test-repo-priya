53ce2dc1dbe9ce3b772c3c5e8b7802176767d861
priya-rani26 <92663484+priya-rani26@users.noreply.github.com>
2021-10-20 15:48:50 +0530

    Delete Student.java

--- Student.java
+++
@@ -1,8 +0,0 @@
-class Student{
-      Pubic  String displayName(){
-            String name="Priya Rani";
-            String name = Student.displayName();
-            System.out.println("my name is : ");
-            return name;
-      }
-}
