128a1e94fa4a0c023e35c789177bf94c640f7143
priya-rani26 <92663484+priya-rani26@users.noreply.github.com>
2021-10-21 13:43:30 +0530

    Student file

--- Student.java
+++ Student.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 public class Student{
    String stuName;
    int addmiNo;
@@ -6,8 +6,6 @@ public class Student{
    String address;
    long mobileNo;
 
-
- 
  public String displayName(){
       String name="Priya Rani";
       return name;
@@ -16,26 +14,9 @@ public class Student{
 
  public static void main(String[] args){
        Student s1=new Student();
-       String name1=s1.displayName();
+       String name1 = s1.displayName();
        System.out.println("My name is "+name1);
    }
    
-
 }        
-=======
-public class Student {
-	String name,branch,emailId,address;
-	int roll,appId;
-	byte age,year;
-	long mobileNo;
-	char section;
-	float fee;
-	public  String displayName() {
-		String studentName="Priya Rani";
-                System.out.println("My name is : ");
-	        return studentName;
-	} 
-	
-	
-}
->>>>>>> f65cc6e8934a47e77830900e77b69d9572fad2c8
+
