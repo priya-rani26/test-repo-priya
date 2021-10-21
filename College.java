04523e05104a46891228406d71355c077432c237
priya-rani26 <priyarani2682002@gmail.com>
2021-10-21 14:35:10 +0530

    file

---
+++ College.java
@@ -0,0 +1,35 @@
+class College {
+	 String name;
+	 int totalStudent;
+         float feeStructure;
+         String courses;
+         String address;
+         long contactNo;
+         String faculty;
+	 
+	     public String collegeName(){
+	        String colName="Bengal College of Engineering";
+	         return colName;
+
+	     }
+
+	     
+	     public String collegeAddress(){
+	         String colAddress ="DA-8, Sahid Sukumar Banerjee Sarani, Bidhannagar, Durgapur, West Bengal 713212";
+	         return colAddress;
+
+	      }
+
+	 
+	     public static void main(String[] args){
+	         College college = new College();
+	         String collegeName1 = college.collegeName();
+	         String collegeAdd = college.collegeAddress();
+	       
+	 	 System.out.println("My college name is "+collegeName1);
+	 	 System.out.println("My college Address is "+collegeAdd);
+		
+
+	      }
+
+	   }
