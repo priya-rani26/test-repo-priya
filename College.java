8fec7370c46e8eb303cbebf9385dbda15de712c7
priya-rani26 <92663484+priya-rani26@users.noreply.github.com>
2021-10-21 15:09:34 +0530

    College file

--- College.java
+++ College.java
@@ -1,35 +1,32 @@
 class College {
-	 String name;
-	 int totalStudent;
-         float feeStructure;
-         String courses;
-         String address;
-         long contactNo;
-         String faculty;
-	 
-	     public String collegeName(){
-	        String colName="Bengal College of Engineering";
-	         return colName;
-
+     String name;
+     int totalStudent;
+     float feeStructure;
+     String courses;
+     String address;
+     long contactNo;
+     int facultyNo;
+     String hodName;	
+     boolean Ispravate;
+	
+	public String collegeName(){
+	      String colName="Bengal College of Engineering";
+	       return colName;
 	     }
-
-	     
-	     public String collegeAddress(){
-	         String colAddress ="DA-8, Sahid Sukumar Banerjee Sarani, Bidhannagar, Durgapur, West Bengal 713212";
-	         return colAddress;
-
+	   
+         public String collegeAddress(){
+	       String colAddress ="DA-8, Sahid Sukumar Banerjee Sarani, Bidhannagar, Durgapur, West Bengal 713212";
+	       return colAddress;
 	      }
 
-	 
-	     public static void main(String[] args){
-	         College college = new College();
-	         String collegeName1 = college.collegeName();
-	         String collegeAdd = college.collegeAddress();
+	  public static void main(String[] args){
+	        College college = new College();
+	        String collegeName1 = college.collegeName();
+	        String collegeAdd = college.collegeAddress();
 	       
-	 	 System.out.println("My college name is "+collegeName1);
+	         System.out.println("My college name is "+collegeName1);
 	 	 System.out.println("My college Address is "+collegeAdd);
 		
-
 	      }
 
 	   }
